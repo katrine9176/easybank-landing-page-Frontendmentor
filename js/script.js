2596958f2e076975551d86e4999efbcee447ef97
Katarzyna Łojas <kaskol.lojas@onet.pl>
2023-01-15 21:49:50 +0100

    fixed nav, added README

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const burgerBtn = document.querySelector('.nav__burger')
 const Xicon = document.querySelector('.nav__Xicon')
 const navLinksBox = document.querySelector('.nav__links')
 const navLinks = document.querySelectorAll('.nav__links-link')
+const header = document.querySelector('.header')
 
 const toggleNav = () => {
     burgerBtn.classList.toggle('active')
@@ -13,6 +14,14 @@ const toggleNav = () => {
             Xicon.classList.add('hide')
         })
     })
+
+    if(navLinksBox.classList.contains('active-nav')) {
+        header.style.opacity = '20%'
+    } else {
+        header.style.opacity = '1'
+    }
+
+
 }
 
 burgerBtn.addEventListener('click', toggleNav)
